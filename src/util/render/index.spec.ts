46f0528038c33014f6fdfc43921ec12bf1ee0dda
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2023-04-08 14:00:40 +0200

    feat(render): add CSS injection

--- src/util/render/index.spec.ts
+++ src/util/render/index.spec.ts
@@ -1,5 +1,5 @@
 import { readFile } from 'fs/promises';
-import { addAtBodyTop } from '.';
+import { addAtBodyTop, addStyles } from '.';
 import { prettify } from '../prettier';
 
 describe('HTML rendering', () => {
@@ -10,6 +10,16 @@ describe('HTML rendering', () => {
     const expected = await readFile('./test/html/after-snip.html', 'utf-8');
     const actual = addAtBodyTop(html, snip);
 
+    expect(await prettify(actual, { parser: 'html' })).toEqual(expected);
+  });
+
+  it('should add CSS to the very end of the document head', async () => {
+    const html = await readFile('./test/html/before-snip.html', 'utf-8');
+    const css =
+      '.active {color: white; font-size: 3em;} body.active div {background-color: #aaa;}';
+
+    const expected = await readFile('./test/html/with-css.html', 'utf-8');
+    const actual = addStyles(html, css);
     expect(await prettify(actual, { parser: 'html' })).toEqual(expected);
   });
 });
--- src/util/render/index.ts
+++ src/util/render/index.ts
@@ -1,7 +1,15 @@
 export const addAtBodyTop = (document: string, snip: string): string => {
-  const foo = /(.+)?(\<body\>)(.+)?(\<\/body\>)(.+)?/s.exec(document);
-  const [_, ...matches] = foo || [];
+  const regexResult = /(.+)?(\<body\>)(.+)?(\<\/body\>)(.+)?/s.exec(document);
+  const [_, ...matches] = regexResult || [];
 
   matches.splice(2, 0, snip);
+  return matches.join('');
+};
+
+export const addStyles = (document: string, css: string): string => {
+  const regexResult = /(.+)?(\<head\>)(.+)?(\<\/head\>)(.+)?/s.exec(document);
+  const [_, ...matches] = regexResult || [];
+
+  matches.splice(3, 0, '<style>' + css + '</style>');
   return matches.join('');
 };
