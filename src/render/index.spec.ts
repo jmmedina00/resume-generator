0913ed23fd51d51b8285957a0bd074a5a24632cc
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2023-04-08 13:08:47 +0200

    chore: move HTML in-tests to separate files

--- src/render/index.spec.ts
+++ src/render/index.spec.ts
@@ -1,49 +1,15 @@
+import { readFile } from 'fs/promises';
 import { addAtBodyTop } from '.';
 import { prettify } from '../prettier';
 
 describe('HTML rendering', () => {
   it('should add snip to the very begin of document body', async () => {
-    const html = `<html>
-        <head>
-          <title>Test</title>
-        </head>
-        <body>
-          <p>Beginning of document</p>
-          <div>Visit our website</div>
-          <script>
-            alert('Hello world');
-          </script>
-        </body>
-      </html>
-        `;
-    const snip = `<ul>
-        <li>Alpha</li>
-        <li>Beta</li>
-        <li>Charlie</li>
-    </ul>`;
+    const html = await readFile('./test/html/before-snip.html', 'utf-8');
+    const snip = `<ul><li>Alpha</li><li>Beta</li><li>Charlie</li></ul>`;
 
-    const expected = `<html>
-    <head>
-      <title>Test</title>
-    </head>
-    <body>
-      <ul>
-        <li>Alpha</li>
-        <li>Beta</li>
-        <li>Charlie</li>
-      </ul>
-      <p>Beginning of document</p>
-      <div>Visit our website</div>
-      <script>
-        alert('Hello world');
-      </script>
-    </body>
-  </html>
-  `;
-
+    const expected = await readFile('./test/html/after-snip.html', 'utf-8');
     const actual = addAtBodyTop(html, snip);
-    expect(await prettify(actual, { parser: 'html' })).toEqual(
-      await prettify(expected, { parser: 'html' })
-    );
+
+    expect(await prettify(actual, { parser: 'html' })).toEqual(expected);
   });
 });
