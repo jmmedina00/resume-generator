9dd953d97bd43d47d6c2c662c955b1e27e320f6f
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2023-04-16 15:39:04 +0200

    chore: add transform tasks

---
+++ src/task/transform.spec.ts
@@ -0,0 +1,186 @@
+import { getFlattenedObjectAndLocales } from '../mapping/locale';
+import { LocalisedObject } from '../mapping/locale.types';
+import {
+  addGitHubInfoToBasics,
+  getDekeyedSectionFromObject,
+  getTranslated,
+} from '../resume/gen-public';
+import { ResumeContext, initialContext } from './context';
+import {
+  transformAndReplaceLocalisedField,
+  transformCompleteToLocalised,
+  transformIncompleteField,
+  transformLocalisedToTranslated,
+} from './transform';
+
+jest.mock('../resume/gen-public'); // Just check types fit target functions
+jest.mock('../mapping/locale');
+
+describe('Transforming tasks', () => {
+  it('should transform incomplete field and append them to complete section', async () => {
+    (addGitHubInfoToBasics as jest.Mock).mockResolvedValue({
+      foo: 'test',
+      bar: 'le',
+    });
+
+    const context: ResumeContext = {
+      ...initialContext,
+      githubUser: {
+        user: 'juanmim',
+        fullName: 'Juanmi Medina',
+        profileUrl: 'this',
+        avatarUrl: 'that',
+      },
+      incomplete: {
+        basics: { needsHelp: true },
+      },
+      complete: {
+        other: 'stuff',
+      },
+    };
+
+    const expectedFinalContext: ResumeContext = {
+      ...context,
+      complete: {
+        other: 'stuff',
+        basics: {
+          foo: 'test',
+          bar: 'le',
+        },
+      },
+    };
+
+    const task = transformIncompleteField('basics', addGitHubInfoToBasics);
+    await task(context);
+
+    expect(context).toEqual(expectedFinalContext);
+    expect(addGitHubInfoToBasics).toHaveBeenCalledWith(
+      { needsHelp: true },
+      {
+        user: 'juanmim',
+        fullName: 'Juanmi Medina',
+        profileUrl: 'this',
+        avatarUrl: 'that',
+      }
+    );
+  });
+
+  it('should consolidate complete sections into localised object', async () => {
+    const context: ResumeContext = {
+      ...initialContext,
+      complete: {
+        basics: { foo: 'bar' },
+        languages: [{ name: 'English', learnedAt: 'foo' }],
+        projects: { calculator: { foo: 'qwe' }, display: { foo: 'asd' } },
+        skills: ['coding', 'fishing'],
+        work: { dev: { test: 'foo' }, consult: { test: 'bar' } },
+        education: { school: { name: 'test' } },
+        irrevelant: 'foo',
+      },
+    };
+
+    const localised: LocalisedObject = {
+      flattened: { patched: 'patched' },
+      locales: { en: { patched: 'Yes' }, es: { patched: 'Sí' } },
+    };
+
+    (getFlattenedObjectAndLocales as jest.Mock).mockReturnValue(localised);
+
+    const expectedFinalContext = {
+      ...context,
+      localised: { ...localised },
+    };
+
+    await transformCompleteToLocalised(context);
+    expect(context).toEqual(expectedFinalContext);
+    expect(getFlattenedObjectAndLocales).toHaveBeenCalledWith({
+      basics: { foo: 'bar' },
+      languages: [{ name: 'English', learnedAt: 'foo' }],
+      projects: { calculator: { foo: 'qwe' }, display: { foo: 'asd' } },
+      skills: ['coding', 'fishing'],
+      work: { dev: { test: 'foo' }, consult: { test: 'bar' } },
+      education: { school: { name: 'test' } },
+    });
+  });
+
+  it('should apply translations into public versions', async () => {
+    const localised: LocalisedObject = {
+      flattened: { test: 'foo' },
+      locales: { en: { test: 'English' }, es: { test: 'Español' } },
+    };
+
+    const translations = {
+      en: { test: 'English-a' },
+      es: { test: 'Español-a' },
+    };
+
+    (getTranslated as jest.Mock).mockReturnValue(translations);
+
+    const context: ResumeContext = {
+      ...initialContext,
+      localised,
+    };
+
+    const expectedFinalContext: ResumeContext = {
+      ...context,
+      publicVersions: { ...translations },
+    };
+
+    await transformLocalisedToTranslated(context);
+    expect(context).toEqual(expectedFinalContext);
+    expect(getTranslated).toHaveBeenCalledWith({ ...localised });
+  });
+
+  it('should patch public versions with dekeyed field', async () => {
+    const localised: LocalisedObject = {
+      flattened: { test: 'foo' },
+      locales: { en: { test: 'English' }, es: { test: 'Español' } },
+    };
+
+    const translations = {
+      en: { test: 'English-a' },
+      es: { test: 'Español-a' },
+    };
+
+    const dekeyed: LocalisedObject = {
+      flattened: { mode: 'dekeyed' },
+      locales: { en: { text: 'test' }, es: { text: 'prueba' } },
+    };
+
+    const translatedField = {
+      en: { foo: '123', bar: '456' },
+      es: { foo: 'qwe', bar: 'asd' },
+    };
+
+    const expectedTranslations = {
+      en: { test: { foo: '123', bar: '456' } },
+      es: { test: { foo: 'qwe', bar: 'asd' } },
+    };
+
+    (getDekeyedSectionFromObject as jest.Mock).mockReturnValue(dekeyed);
+    (getTranslated as jest.Mock).mockReturnValue(translatedField);
+
+    const context: ResumeContext = {
+      ...initialContext,
+      localised,
+      publicVersions: { ...translations },
+    };
+
+    const expectedFinalContext: ResumeContext = {
+      ...initialContext,
+      localised,
+      publicVersions: { ...expectedTranslations },
+    };
+
+    const task = transformAndReplaceLocalisedField('test', ['foo', 'bar']);
+    await task(context);
+
+    expect(context).toEqual(expectedFinalContext);
+    expect(getTranslated).toHaveBeenCalledWith(dekeyed);
+    expect(getDekeyedSectionFromObject).toHaveBeenCalledWith(
+      { ...localised },
+      'test',
+      ['foo', 'bar']
+    );
+  });
+});
---
+++ src/task/transform.ts
@@ -0,0 +1,54 @@
+import { getFlattenedObjectAndLocales } from '../mapping/locale';
+import {
+  getDekeyedSectionFromObject,
+  getTranslated,
+} from '../resume/gen-public';
+import { GithubUserInfo } from '../service/github';
+import { ResumeContext } from './context';
+
+export const transformIncompleteField =
+  (
+    field: string,
+    func: (contents: any, user: GithubUserInfo) => Promise<any>
+  ) =>
+  async (ctx: ResumeContext): Promise<void> => {
+    const incomplete = ctx.incomplete[field];
+    ctx.complete[field] = await func(incomplete, ctx.githubUser);
+  };
+
+export const transformCompleteToLocalised = async (
+  ctx: ResumeContext
+): Promise<void> => {
+  const { basics, languages, projects, skills, work, education } = ctx.complete;
+  ctx.localised = getFlattenedObjectAndLocales({
+    basics,
+    languages,
+    projects,
+    skills,
+    work,
+    education,
+  });
+};
+
+export const transformLocalisedToTranslated = async (
+  ctx: ResumeContext
+): Promise<void> => {
+  ctx.publicVersions = getTranslated(ctx.localised);
+};
+
+export const transformAndReplaceLocalisedField =
+  (field: string, defaultedSubFields: string[]) =>
+  async (ctx: ResumeContext): Promise<void> => {
+    const translatedField = getTranslated(
+      getDekeyedSectionFromObject(ctx.localised, field, defaultedSubFields)
+    );
+
+    const patchedPublicVersions = Object.fromEntries(
+      Object.entries(ctx.publicVersions).map(([code, object]) => [
+        code,
+        { ...object, [field]: translatedField[code] },
+      ])
+    );
+
+    ctx.publicVersions = patchedPublicVersions;
+  };
