ce6390578492ed7043165a91a4acee900a0e2e32
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2023-05-07 11:17:39 +0200

    fix: ensure writing to file always has directory existing

--- src/task/io/write.spec.ts
+++ src/task/io/write.spec.ts
@@ -1,11 +1,12 @@
-import { writeFile } from 'fs/promises';
+import { mkdir, writeFile } from 'fs/promises';
 import { ResumeContext, initialContext } from '../context';
 import { writeToFile } from './write';
+import { dirname } from 'path';
 
 jest.mock('fs/promises');
 
 describe('File writing', () => {
-  it('should be able to read as specified by context function into designated path', async () => {
+  it('should be able to write as specified by context function into designated path', async () => {
     const context: ResumeContext = {
       ...initialContext,
       incomplete: { foo: 'bar', bar: 'baz' },
@@ -22,5 +23,20 @@ describe('File writing', () => {
       JSON.stringify({ foo: 'bar', bar: 'baz' }),
       'utf-8'
     );
+  });
+
+  it('should ensure directory exists before writing', async () => {
+    const context: ResumeContext = {
+      ...initialContext,
+      incomplete: { foo: 'bar', bar: 'baz' },
+    };
+
+    const path = './foo/bar/baz.txt';
+    const fn = jest.fn();
+
+    const task = writeToFile(path, fn);
+    await task(context);
+
+    expect(mkdir).toHaveBeenCalledWith(dirname(path), { recursive: true });
   });
 });
--- src/task/io/write.ts
+++ src/task/io/write.ts
@@ -1,5 +1,6 @@
-import { writeFile } from 'fs/promises';
+import { mkdir, writeFile } from 'fs/promises';
 import { ResumeContext } from '../context';
+import { dirname } from 'path';
 
 export type FileWriter = (
   path: string,
@@ -8,5 +9,6 @@ export type FileWriter = (
 
 export const writeToFile: FileWriter = (path, fn) => async (ctx) => {
   const contents = fn(ctx);
+  await mkdir(dirname(path), { recursive: true });
   await writeFile(path, contents, 'utf-8');
 };
