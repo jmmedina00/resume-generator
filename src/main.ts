7ff81f4237832c54068cf19772de3f96b190803b
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2023-05-14 13:33:25 +0200

    chore: remove pre-task generation code

--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,9 @@
 import dotenv from 'dotenv';
-import { makeResumes } from './resume/gen-public';
-import { promisify } from 'util';
-import { getPrivateVersionGenerator } from './resume/gen-private';
-import { EncryptedData, decryptText } from './util/encrypt';
-import { getFileContents } from './service/gdrive';
-import { parse } from 'yaml';
 import { tasks } from './task';
-
-const schema = require('resume-schema');
 
 const main = async () => {
   dotenv.config();
   await tasks.run();
-
-  return;
-  const validate = promisify(schema.validate);
-
-  const resumes = await makeResumes('./resume.yml');
-  const validations = resumes.map(async ([code, resume]) => [
-    code,
-    await validate(resume),
-  ]);
-
-  console.log(await Promise.all(validations));
-
-  const fileId = process.env['PRIVATE_FILE_ID'] || '';
-  const encrypted: EncryptedData = await getFileContents(fileId);
-  const privateIterations: any[] = parse(decryptText(encrypted));
-  const privateGenerator = await getPrivateVersionGenerator(privateIterations);
-
-  const privateResumes = resumes.map<[string, object[]]>(([code, resume]) => [
-    code,
-    privateGenerator(resume),
-  ]);
-
-  const privateValidations = privateResumes.map(
-    async ([code, privateCollection]) => [
-      code,
-      await Promise.all(privateCollection.map((resume) => validate(resume))),
-    ]
-  );
-
-  console.log(JSON.stringify(await Promise.all(privateValidations)));
 };
 
 main();
--- src/resume/gen-public.ts
+++ src/resume/gen-public.ts
@@ -1,9 +1,6 @@
-import { readFile } from 'fs/promises';
-import { parse } from 'yaml';
-import { GithubUserInfo, getCoreUserInfo } from '../service/github';
+import { GithubUserInfo } from '../service/github';
 import { PartialProfiles, getFullProfiles } from './profile';
 import { ResumeProject, getResumeProject } from './project';
-import { getFlattenedObjectAndLocales } from '../mapping/locale';
 import { dekeyObject } from '../mapping/dekey';
 import { extractKey } from '../mapping/extract';
 import { LocalisedObject } from '../mapping/locale.types';
@@ -60,49 +57,3 @@ export const getDekeyedSectionFromObject = (
   field: string,
   defaultedSubFields: string[]
 ) => dekeyObject(extractKey(localised, field), defaultedSubFields);
-
-export const makeResumes = async (path: string) => {
-  // TODO transfer all this logic to proper task runner
-
-  const resumeFile = await readFile(path, 'utf-8');
-  const {
-    basics: incompleteBasics,
-    languages,
-    projects: keyedProjects,
-    skills,
-    work,
-    education,
-  } = parse(resumeFile);
-
-  const githubUser = await getCoreUserInfo();
-  const basics = await addGitHubInfoToBasics(incompleteBasics, githubUser);
-  const projects = getProperProjects(keyedProjects, githubUser);
-
-  const localised = getFlattenedObjectAndLocales({
-    basics,
-    languages,
-    projects,
-    skills,
-    work,
-    education,
-  });
-
-  const translated = getTranslated(localised);
-
-  const translatedWork = getTranslated(
-    getDekeyedSectionFromObject(localised, 'work', ['position', 'summary'])
-  );
-
-  const translatedEducation = getTranslated(
-    getDekeyedSectionFromObject(localised, 'education', ['area', 'studyType'])
-  );
-
-  return Object.entries(translated).map<[string, object]>(([code, object]) => [
-    code,
-    {
-      ...object,
-      work: translatedWork[code],
-      education: translatedEducation[code],
-    },
-  ]);
-};
