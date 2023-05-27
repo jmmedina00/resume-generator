7fcb278b66f46daed1d2d94f5727bd86612b241a
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2023-05-27 11:55:07 +0200

    feat(resume): enable appending values to fields in private iterations

--- src/resume/gen-private.spec.ts
+++ src/resume/gen-private.spec.ts
@@ -88,4 +88,131 @@ describe('Private version generation', () => {
     const actual = getPrivateVersionGenerator(privateIterations)({ basics });
     expect(actual).toEqual(expected);
   });
+
+  it('should use the append field to add to plain fields', () => {
+    const basics = {
+      summary: "I'm a potato",
+      label: 'Veggie',
+      foo: 'bar',
+    };
+
+    const privateIterations = [
+      {
+        location: 'Wherever',
+        append: {
+          label: ' that is brown',
+        },
+      },
+      {
+        phone: '555-5118',
+        append: {
+          summary: '-harvester',
+        },
+      },
+    ];
+
+    const expected = [
+      {
+        basics: {
+          summary: "I'm a potato",
+          label: 'Veggie that is brown',
+          foo: 'bar',
+          location: 'Wherever',
+        },
+      },
+      {
+        basics: {
+          summary: "I'm a potato-harvester",
+          label: 'Veggie that is brown',
+          foo: 'bar',
+          location: 'Wherever',
+          phone: '555-5118',
+        },
+      },
+    ];
+    const actual = getPrivateVersionGenerator(privateIterations)({ basics });
+    expect(actual).toEqual(expected);
+  });
+
+  it("should be able to copy append field value if it doesn't exist yet", () => {
+    const basics = {
+      summary: "I'm a potato",
+      label: 'Veggie',
+      foo: 'bar',
+    };
+
+    const privateIterations = [
+      {
+        location: 'Wherever',
+        append: {
+          label: ' that is brown',
+          bar: 'baz',
+        },
+      },
+    ];
+
+    const expected = [
+      {
+        basics: {
+          summary: "I'm a potato",
+          label: 'Veggie that is brown',
+          foo: 'bar',
+          location: 'Wherever',
+          bar: 'baz',
+        },
+      },
+    ];
+    const actual = getPrivateVersionGenerator(privateIterations)({ basics });
+    expect(actual).toEqual(expected);
+  });
+
+  it('should not admit append section with object fields', () => {
+    const basics = {
+      summary: "I'm a potato",
+      label: 'Veggie',
+      foo: 'bar',
+    };
+
+    const privateIterations = [
+      {
+        location: 'Wherever',
+        append: {
+          label: ' that is brown',
+        },
+      },
+      {
+        phone: '555-5118',
+        append: {
+          summary: '-harvester',
+          donot: { do: 'this' },
+        },
+      },
+    ];
+
+    expect(() => {
+      getPrivateVersionGenerator(privateIterations)({ basics });
+    }).toThrow();
+  });
+
+  it('should not be able to append to complex field', () => {
+    const basics = {
+      test: 'foo',
+      location: {
+        name: 'Exist',
+        city: 'Malaga',
+      },
+    };
+
+    const privateIterations = [
+      {
+        append: {
+          location: 'Wut',
+        },
+      },
+    ];
+
+    expect(() => {
+      getPrivateVersionGenerator(privateIterations)({ basics });
+    }).toThrow();
+  });
 });
--- src/resume/gen-private.ts
+++ src/resume/gen-private.ts
@@ -1,9 +1,38 @@
-export const getPrivateVersionGenerator = (privateIterations: object[]) => {
+const appendValuesToObject = (basics: any, append: any) => {
+  if (!!Object.values(append).find((value) => value instanceof Object))
+    throw new Error('No complex objects to be appended');
+
+  const set = new Set([...Object.keys(basics), ...Object.keys(append)]);
+
+  const entries: [key: string, value: any][] = [...set].map((key) => {
+    const basicValue = basics[key];
+    const appendValue = append[key];
+
+    if (!appendValue) return [key, basicValue];
+    if (!basicValue) return [key, appendValue];
+
+    if (basicValue instanceof Object && !!appendValue)
+      throw new Error('Cannot append to object');
+
+    return [key, basicValue + appendValue];
+  });
+
+  return Object.fromEntries(entries);
+};
+
+export const getPrivateVersionGenerator = (privateIterations: any[]) => {
   return ({ basics: publicBasics, ...resume }: { [key: string]: any }) => {
     const privateBasics = privateIterations.reduce<object[]>(
-      (list, current, index) => {
+      (list, { append, ...current }, index) => {
         const previous = index === 0 ? publicBasics : list[index - 1];
-        return [...list, { ...previous, ...current }];
+        const currentBasics = { ...previous, ...current };
+
+        return [
+          ...list,
+          !!append
+            ? appendValuesToObject(currentBasics, append)
+            : currentBasics,
+        ];
       },
       []
     );
